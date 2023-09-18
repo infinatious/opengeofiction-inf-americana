53aa3d0f4f324a4f54f74ad2f8a1c00f48f77341
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2023-09-19 03:53:37 +0800

    Change americana config to OGF

--- src/config.js
+++ src/config.js
@@ -1,14 +1,40 @@
-// The build script injects this global variable if "local.config.js"
-// exists in the project root (alongside package.json). That file is in
-// gitignore and so can be modified without making changes to this file.
+"use strict";
 
-const importConfig = () => {
-  if (typeof CONFIG_PATH !== "undefined") {
-    return import(CONFIG_PATH);
-  } else {
-    return import("./configs/config.aws.js");
-  }
+/*
+This style requires a vector tile server to run.
+This configuration is for running with MapTiler Cloud.
+
+Visit MapTiler Cloud and create an account/log in:
+https://cloud.maptiler.com/maps/
+
+Go to Account->Keys and create a key to paste here:
+*/
+//const MAPTILER_KEY = "<your MapTiler key>";
+
+const OPENMAPTILES_URL = `https://ogfvector.infinatio.us/data/openmaptiles.json`
+const ATTRIBUTION_LOGO = `
+<a href="https://opengeofiction.net">
+  <img src="https://opengeofiction.net/assets/osm_logo-d621af7a73a07ad6abb9617a9ab397682b788b9d90221afa998a0f1744b7295a.svg"
+  alt="OGF logo"
+  width=40
+  style="padding: 10px;"
+  />
+</a>`;
+const ATTRIBUTION_TEXT =
+  '<a href="https://opengeofiction.net" target="_blank">&copy; OpenGeofiction contributors</a>';
+
+/*
+The following two variables override the color of the bounding box and halo of
+shield text, respectively. Useful while testing shield design changes.
+Both accept an HTML color name, hex code, or other CSS color value.
+*/
+const SHIELD_TEXT_BBOX_COLOR = null;
+const SHIELD_TEXT_HALO_COLOR_OVERRIDE = null;
+
+export default {
+  OPENMAPTILES_URL,
+  ATTRIBUTION_LOGO,
+  ATTRIBUTION_TEXT,
+  SHIELD_TEXT_BBOX_COLOR,
+  SHIELD_TEXT_HALO_COLOR_OVERRIDE,
 };
-
-const { default: config } = await importConfig();
-export { config as default };
